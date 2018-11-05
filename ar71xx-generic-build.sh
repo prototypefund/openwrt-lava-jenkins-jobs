3e6c949135788bcd8eb8b9f15e9809d1c53c33dc
Alexander Couzens <lynxis@fe80.eu>
2018-11-05 08:38:16 +0100

    initial

---
+++ ar71xx-generic-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+[ -z "$CPUS" ] && CPUS="$(nproc)"
+
+cat >.config <<EOF
+CONFIG_TARGET_ar71xx=y
+CONFIG_TARGET_ar71xx_generic=y
+CONFIG_TARGET_MULTI_PROFILE=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_archer-c7-v2=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_archer-c7-v4=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr1043n-v5=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr1043nd-v1=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr1043nd-v2=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr1043nd-v3=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr1043nd-v4=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr841-v10=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr841-v11=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr841-v12=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr841-v8=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wr841-v9=y
+CONFIG_DEVEL=y
+CONFIG_BUILD_LOG=y
+CONFIG_TARGET_INITRAMFS_COMPRESSION_LZMA=y
+CONFIG_TARGET_ROOTFS_INITRAMFS=y
+EOF
+
+make defconfig && make -j "$CPUS"
---
+++ ar71xx-generic-lava.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 2018 Alexander Couzens <lynxis@fe80.eu>
+# GPLv2
+
+# optional envs:
+#
+# LAVA_USER
+# LAVA_URL
+#
+# required envs:
+#
+# BOARD - defining the board to test
+# IMAGE_URL - url to the an image (e.g. produced by jenkins)
+
+export PATH=$PATH:/usr/local/bin
+
+if [ ! -e "job-templates/$BOARD.yml" ] ; then
+	echo "Can not find the template"
+	exit 127
+fi
+
+TEMPLATE_FILE="job-templates/$BOARD.yml"
+if [ ! -e "$TEMPLATE_FILE" ] ; then
+	echo "Can not find the template file '$TEMPLATE_FILE'"
+fi
+
+IMAGE_NAME_FILE="boards/$BOARD.image.initramfs"
+if [ ! -e "$IMAGE_NAME_FILE" ] ; then
+	echo "Can not find the image name file '$IMAGE_NAME_FILE'"
+fi
+
+IMAGE_NAME="$(cat "$IMAGE_NAME_FILE")"
+export IMAGE_URL="$IMAGE_URL$IMAGE_NAME"
+export JOB_NAME="jenkins_$BUILD_NUMBER"
+export TEMPLATE_FILE
+export LAVA_USER
+
+run-lava-job.sh
